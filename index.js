3f78dee8057272d792c5852776f858567dc61003
Paras Srivastav <paras.sri50@gmail.com>
2019-12-04 13:41:05 +0530

    initial

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
+var socket=void 0;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function(_socket){
+  socket=_socket;
 });
 
+let i = 0;
+setInterval(function() {
+  socket &&    socket.emit('increase', {val:i});
+  i++;
+},1000)
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+  console.log('listening on :' + port);
 });
---
+++ readme.md
@@ -0,0 +1,1 @@
+A simple socket IO example
